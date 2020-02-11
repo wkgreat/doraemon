fca28ce7787cf22586ec9e3c90b2e0cb72937dd3
wkgreat <wkgreat@outlook.com>
2020-02-11 20:06:01 +0800

    feat(geo): locate a pont to a city.
    locate a pont to a city.
    find the administrative info of city

--- src/main/java/wk/doraemon/geo/regions/AdCodes.java
+++ src/main/java/wk/doraemon/geo/regions/AdCodes.java
@@ -7,13 +7,16 @@ package wk.doraemon.geo.regions;
 import wk.doraemon.io.TextReader;
 
 import java.io.InputStream;
+import java.io.Serializable;
 import java.util.*;
 
-public class AdCodes {
+public class AdCodes implements Serializable {
 
-    private static List<List<String>> records;
+    private List<List<String>> records;
+    private enum FIELDS {PROV_ID,PROV_NAME,CITY_ID,CITY_NAME,DISTRICT_ID,DISTRICT_NAME,PHONE_CODE};
 
-    static {
+    //prov_id,prov_name,city_id,city_name,district_id,district_name,phone_code
+    public AdCodes() {
         InputStream is = AdCodes.class.getClassLoader().getResourceAsStream("adcodes.csv");
         TextReader reader = new TextReader(is).init();
         reader.readlines();
@@ -25,7 +28,7 @@ public class AdCodes {
      * 获取每个省所有的城市的编码
      * @return Map：key为中国每个省的中文名称，value为List对应每个省所有城市的编码，其中城市编码在List中是有序的
      * */
-    public static Map<String,List<String>> getProvinceCodeCityCodes() {
+    public Map<String,List<String>> getProvinceCodeCityCodes() {
 
         Map<String, Set<String>> pcMap = new HashMap<>();
         records.forEach(r->{
@@ -49,7 +52,7 @@ public class AdCodes {
      * 获取每个省所有的城市的编码
      * @return Map：key为中国每个省的编码，value为List对应每个省所有城市的编码，其中城市编码在List中是有序的
      * */
-    public static Map<String,List<String>> getProvinceNameCityCodes() {
+    public Map<String,List<String>> getProvinceNameCityCodes() {
 
         Map<String, Set<String>> pcMap = new HashMap<>();
         records.forEach(r->{
@@ -70,7 +73,7 @@ public class AdCodes {
         return pcMap2;
     }
 
-    public static String provinceNameToCode(String pname) {
+    public String provinceNameToCode(String pname) {
         String pcode = "";
         for(List<String> r : records) {
             if(r.get(1).equals(pname)) return r.get(0);
@@ -78,7 +81,7 @@ public class AdCodes {
         return pcode;
     }
 
-    public static String provinceCodeToName(String pcode) {
+    public String provinceCodeToName(String pcode) {
         String pname = "";
         for(List<String> r : records) {
             if(r.get(0).equals(pcode)) return r.get(1);
@@ -86,6 +89,22 @@ public class AdCodes {
         return pname;
     }
 
+    public List<String> getCityInfoByCode(String cityCode) {
+        List<String> record = new ArrayList<>();
+        for(List<String> r : records) {
+            if(r.get(2).equals(cityCode)) {
+                record = r;
+                break;
+            }
+        }
+        return record;
+    }
 
+
+    public static void main(String[] args) {
+        AdCodes adCodes = new AdCodes();
+        List<String> info = adCodes.getCityInfoByCode("341500");
+        System.out.println(info);
+    }
 
 }
---
+++ src/main/java/wk/doraemon/geo/regions/CityBoudary.java
@@ -0,0 +1,135 @@
+/**
+ * Created by ke.wang7 on 2019/12/30.
+ */
+package wk.doraemon.geo.regions;
+
+import org.geotools.data.DataStore;
+import org.geotools.data.DataStoreFinder;
+import org.geotools.data.FeatureSource;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.Point;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.filter.Filter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wk.doraemon.geo.JTSUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CityBoudary implements Serializable {
+
+    private final static Logger LOG = LoggerFactory.getLogger(CityBoudary.class);
+
+    /**
+     * 城市边界polygon的shapefile数据
+     * */
+    private String cityBoundarySHPPath;
+
+    /**
+     * 各市的polygon几何对象
+     * */
+    public Map<String, Geometry> cityGeometries = null;
+
+    public CityBoudary(String shpPath) {
+        cityBoundarySHPPath = shpPath;
+        getCityGeometries();
+    }
+
+    /**
+     * 从shapefile中读取各市的polygon几何对象
+     * */
+    private Map<String, Geometry> getCityGeometries(){
+
+        if(cityGeometries!=null){
+            return cityGeometries;
+        }
+        else {
+            try {
+                cityGeometries = new ConcurrentHashMap<>();
+
+                File file = new File(cityBoundarySHPPath);
+                Map<String,Object> map = new HashMap<>();
+                map.put("url",file.toURI().toURL());
+                DataStore dataStore = DataStoreFinder.getDataStore(map);
+                String typeName = dataStore.getTypeNames()[0];
+
+                FeatureSource<SimpleFeatureType, SimpleFeature> source =
+                        dataStore.getFeatureSource(typeName);
+                Filter filter = Filter.INCLUDE; // ECQL.toFilter("BBOX(THE_GEOM, 10,20,30,40)")
+
+                FeatureCollection<SimpleFeatureType, SimpleFeature> collection = source.getFeatures(filter);
+                try (FeatureIterator<SimpleFeature> features = collection.features()) {
+                    while (features.hasNext()) {
+                        SimpleFeature feature = features.next();
+                        String citycode = feature.getProperty("CITYCODE").getValue().toString();
+                        Geometry geometry = (Geometry) feature.getDefaultGeometry();
+                        geometry.setSRID(4326);
+                        cityGeometries.put(citycode,geometry);
+                    }
+                }
+            } catch (IOException e) {
+                LOG.error("Read ShapeFile Error: {}", e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        return cityGeometries;
+    }
+
+    public String locateCity(Point point) {
+        for(Map.Entry<String, Geometry> entry : getCityGeometries().entrySet()) {
+
+            String citycode = entry.getKey();
+            Geometry cityGeom = entry.getValue();
+            if(point.intersects(cityGeom)) {
+                return citycode;
+            }
+        }
+        return "";
+    }
+
+    /**
+     * 计算一条路线在每个市经过的距离
+     * */
+    public Map<String, Double> cityDistance(Geometry route) {
+
+        Map<String, Double> cityDist = new HashMap<>();
+
+        for(Map.Entry<String, Geometry> entry : getCityGeometries().entrySet()) {
+
+            String citycode = entry.getKey();
+            Geometry cityGeom = entry.getValue();
+            if(route.intersects(cityGeom)) {
+                Geometry geom = cityGeom.intersection(route);
+                double dist = JTSUtils.getLengthInMeters(geom);
+                cityDist.put(citycode, dist);
+            }
+        }
+        return cityDist;
+    }
+
+    public double getTotalDistance(Map<String,Double> cityDists) {
+        double d = 0;
+        for(Map.Entry<String,Double> entry: cityDists.entrySet()) {
+            d += entry.getValue();
+        }
+        return d;
+    }
+
+    public static void main(String[] args) {
+
+        CityBoudary b = new CityBoudary("/Users/wkgreat/codes/projects/doraemon/src/main/resources/china_ad_boundary_WGS1984/city_boudary.shp");
+        //System.out.println(b.cityGeometries);
+        String code = b.locateCity(JTSUtils.getPoint(117,32,4326));
+        System.out.println(code);
+
+    }
+
+}
